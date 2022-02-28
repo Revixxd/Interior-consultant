6c3c58e6a6048bbd8b522ee629480746233241c4
Adam Myślicki <revixxdm@gmail.com>
2022-02-28 21:12:16 +0100

    hamburger menu
    things to do:
    
    -animate showing element of list
    -change icons for hamburger menu
    -add after div on selected category

--- scripts/overlayMenu.js
+++ scripts/overlayMenu.js
@@ -2,10 +2,22 @@ let overLay = document.querySelector(".menu_overlay")
 let on = document.querySelector(".header-overLay__button")
 let off = document.querySelector('.menu-overlay__button')
 
+
 on.addEventListener('click', ()=>{
     overLay.style.transform = "translateY(0)"
 })
 off.addEventListener('click', ()=>{
-    overLay.style.transform = "translateY(-120%)"
+    offOverlay()
 })
 
+function offOverlay(){
+    overLay.style.transform = "translateY(-120%)"
+}
+
+let aButtons = document.querySelectorAll('.menu_overlay-list__button')
+
+aButtons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        offOverlay()
+    })
+})
